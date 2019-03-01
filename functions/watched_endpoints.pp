92cb0b59e48beb077f9a08265ee1cc740603e7d5
Romain Tartière <romain@blogreen.org>
2019-03-01 11:26:24 -1000

    Improve function idempotency
    PuppetDB results are unordered by default, so the value returned by
    tls_checker::watched_endpoints() may change from time to time.  To avoid
    this, remove duplicate items and sort the array alphabetically.

--- functions/watched_endpoints.pp
+++ functions/watched_endpoints.pp
@@ -1,4 +1,4 @@
 function tls_checker::watched_endpoints() >> Array[String] {
   $pql = "resources [parameters] { type = 'Tls_checker::Watch' and certname = '${trusted['certname']}' }"
-  puppetdb_query($pql).map |$value| { $value.dig('parameters', 'endpoints') }.flatten()
+  puppetdb_query($pql).map |$value| { $value.dig('parameters', 'endpoints') }.flatten().unique().sort()
 }
