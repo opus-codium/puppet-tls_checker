6a5fefd538357b3216edb989e5043cb813a69d7d
Romain Tartière <romain@blogreen.org>
2020-01-03 08:12:53 -1000

    Add doc

--- functions/watched_endpoints.pp
+++ functions/watched_endpoints.pp
@@ -1,3 +1,6 @@
+# @summary Extract watched TLS endpoints from PuppetDB
+#
+# @return [Array[String]] An array of TLS endpoints
 function tls_checker::watched_endpoints() >> Array[String] {
   $pql = "resources [parameters] { type = 'Tls_checker::Watch' and certname = '${trusted['certname']}' }"
   puppetdb_query($pql).map |$value| { $value.dig('parameters', 'endpoints') }.flatten.unique.sort
