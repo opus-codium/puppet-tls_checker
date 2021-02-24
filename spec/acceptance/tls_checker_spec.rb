ed96b7dd1d33e6d7363a73d7c1ff50dfc4ae3b7d
Romain Tartière <romain@blogreen.org>
2021-02-23 14:38:27 -1000

    Add acceptance tests

---
+++ spec/acceptance/tls_checker_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper_acceptance'
+
+describe 'tls_checker class' do
+  it 'works idempotently with no errors' do
+    pp = <<~MANIFEST
+      function puppetdb_query(String $pql) {
+        []
+      }
+
+      class { 'tls_checker':
+      }
+    MANIFEST
+
+    apply_manifest(pp, catch_failures: true)
+    apply_manifest(pp, catch_changes: true)
+  end
+end
